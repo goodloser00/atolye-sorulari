eb1ca88801c170af11b972b705d864edf6ccd473
goodloser00 <48631041+goodloser00@users.noreply.github.com>
2019-03-16 21:31:42 +0200

    Update Nedfliks-Kupon.php

--- Nedfliks-Kupon.php
+++ Nedfliks-Kupon.php
@@ -1,11 +1,11 @@
 class Kupon {
  public $deger = '';
- public $omurboyu = '0';
+ public $omurboyu = '1000';
 public function degerAta($deger) {
         $this->deger=$deger;
 }
 public function olustur() {
-        if($this->deger == '10')
+        if($this->deger == '1000')
                 echo 'İste kupon kodunuz.'.rand();
         if($this->omurboyu == '1')
                 echo 'FLAG_**';
